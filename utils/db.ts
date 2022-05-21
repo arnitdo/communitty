999395123a73f3901443dcfc7e05bbf88c22acb1
arnitdo <arnav.nitin.deo@gmail.com>
2022-05-21 21:35:47 +0530

    Update db types

--- utils/db.ts
+++ utils/db.ts
@@ -1,8 +1,8 @@
-const pg = require("pg")
+import * as pg from "pg";
+import * as env from "dotenv";
 
 if (process.env.NODE_ENV === "development"){
     // Configure environment in case of local deployments, using .env
-    const env = require("dotenv")
     env.config()
 } else {
     // Do nothing, fetch environment variables from preconfigured deployment environment (e.g Heroku)
@@ -10,9 +10,9 @@ if (process.env.NODE_ENV === "development"){
 
 // Initialize and configure db
 // Using node-postgres (i.e) pg for PSQL connection
-const db = new pg.Pool({
+const db: pg.Pool = new pg.Pool({
     connectionString : process.env.DATABASE_URL
 })
 
 // Finally, export database connection as default export
-module.exports = db
+export {db}
