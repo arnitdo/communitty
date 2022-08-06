e93f85a61196ca1cb40c7990aee017043f9891f1
arnitdo <arnav.nitin.deo@gmail.com>
2022-08-06 14:45:41 +0530

    Add post creation route

---
+++ routes/postRoutes.ts
@@ -0,0 +1,102 @@
+import {db} from '../utils/db'
+import {Request, Response} from 'express'
+import {getAuthenticatedUser} from "../utils/common";
+import * as url from 'node:url'
+
+type PostType = "TEXT_POST" | "LINK_POST" | "IMAGE_POST" | "VIDEO_POST"
+
+function getRandomTagsFromTitle(postTitle: string): string[] {
+    let tags: Set<string> = new Set<string>();
+    postTitle = postTitle.trim()
+
+    // Remove all punctuation marks and other special characters
+    const symbolRegex = /(\.|\s|_|-|;|:|!|\?)/gi
+    const strippedPostTitle = postTitle.replace(symbolRegex, ' ')
+    let titleWords: string[] = strippedPostTitle.split(' ')
+    titleWords.forEach((titleWord: string, wordIndex: number) => {
+        titleWords[wordIndex] = titleWord
+            .toLowerCase()
+            .trim()
+    })
+
+    titleWords = titleWords.filter((titleWord: string) => {
+        if (titleWord == ""){
+            return false
+        }
+        return true
+    })
+
+    if (titleWords.length < 4){
+        return titleWords
+    }
+
+    while(tags.size < 4){
+        const randomWordIndex = Math.floor(Math.random() * titleWords.length)
+        const randomWord = titleWords.at(randomWordIndex)!
+        tags.add(randomWord)
+    }
+
+    const tagList = [...tags]
+    return tagList
+}
+
+async function createPost(req: Request, res: Response): Promise<void> {
+    try {
+        await db.query("BEGIN;")
+        let {postTitle, postBody} = req.body;
+        // Get the post's author from the current authenticated user's token
+        const postAuthor = getAuthenticatedUser(req)
+        const postType: PostType = req.body.postType || "TEXT_POST"
+        if (postType != "TEXT_POST"){
+            try {
+                // Note that the post body must be a valid URL in case of
+                // Link, Image or Video posts
+                // URL.constructor() errs when an invalid URL is provided
+                const bodyURL = new url.URL(postBody)
+
+                // We'll make a test call to the URL. A 404 will result in the post being discarded
+                const linkResponse = await fetch(postBody)
+                if (!linkResponse.ok){
+                    res.status(400).json({
+                        "actionResult": "ERR_INVALID_PROPERTIES",
+                        "invalidProperties": ["postBody"]
+                    })
+                    return
+                }
+            } catch (err){
+                if (err instanceof TypeError){
+                    res.status(400).json({
+                        "actionResult": "ERR_INVALID_PROPERTIES",
+                        "invalidProperties": ["postBody"]
+                    })
+                    return
+                }
+                // Pass it down to the overall handler
+                throw err
+            }
+        }
+        let postTags: string[] = [];
+        let requestTags: string | null | undefined = req.body.postTags
+        if (requestTags != null){
+            postTags = requestTags.trim().split(",")
+        } else {
+            postTags = getRandomTagsFromTitle(postTitle)
+        }
+        await db.query(
+            // post_id DEFAULT SERIAL | post_date_created NOW() | post_like_count DEFAULT 0
+            "INSERT INTO posts VALUES (DEFAULT, $1, $2, $3, $4, $5, NOW(), DEFAULT);",
+            [postAuthor, postType, postTitle, postBody, postTags]
+        )
+        await db.query("COMMIT;")
+        res.status(200).json({
+            "actionResult": "SUCCESS"
+        })
+    } catch (err) {
+        console.error(err)
+        await db.query("ROLLBACK;")
+    }
+}
+
+export {
+    createPost
+}
--- server.ts
+++ server.ts
@@ -1,9 +1,14 @@
 import * as express from "express"
 import * as path from "path"
+import helmet from "helmet"
+
+import {preconfiguredCors} from "./utils/corsPreconfig"
+import {needsBodyParams, needsToken, needsVerifiedUser} from "./utils/common"
+import {db} from "./utils/db";
+
 import * as clientRoutes from "./routes/clientRoutes"
 import * as authRoutes from "./routes/authRoutes"
-import {preconfiguredCors} from "./utils/corsPreconfig"
-import helmet from "helmet"
+import * as postRoutes from "./routes/postRoutes"
 
 // Initialize ExpressJS App
 const app = express()
@@ -28,15 +33,57 @@ app.use(
 
 // Serve client on all routes
 // Client side routing will be handled by react-router
-app.post("/api/auth/user_signup", authRoutes.userSignup)
-app.post("/api/auth/user_verify", authRoutes.userVerify)
-app.post("/api/auth/user_login", authRoutes.userLogin)
-app.post("/api/auth/token_refresh", authRoutes.userTokenRefresh)
+app.post(
+    "/api/auth/user_signup",
+    needsBodyParams("userName", "userMail", "userPass", "fullName"),
+    authRoutes.userSignup
+)
+app.post(
+    "/api/auth/user_activate",
+    needsBodyParams("userName", "activationToken"),
+    authRoutes.userActivate
+)
+app.post(
+    "/api/auth/user_login",
+    needsBodyParams("userName",  "userPass"),
+    authRoutes.userLogin
+)
+
+app.post(
+    "/api/auth/token_refresh",
+    needsBodyParams("authToken", "refreshToken"),
+    authRoutes.userTokenRefresh
+)
+
+app.post(
+    "/api/posts/new",
+    [
+        needsToken,
+        needsVerifiedUser,
+        needsBodyParams("postTitle", "postType", "postBody") // "postTags" is optional here!
+    ],
+    postRoutes.createPost
+)
 
 app.get("*", clientRoutes.serveClient)
 
 // Serve app on production port
 // Empty callback as of now
-app.listen(process.env.PORT || 8800, () => {
+const appServer = app.listen(process.env.PORT || 8800, () => {
     console.log("Communitty backend server is up and running!")
+})
+
+// Many IDEs send SIGINT to processes
+process.on("SIGINT", () => {
+    appServer.close()
+})
+
+// Heroku sends SIGTERM to processes
+process.on("SIGTERM", () => {
+    appServer.close()
+})
+
+appServer.on('close', async () => {
+    await db.end()
+    console.log("Stopping communitty backend server")
 })
