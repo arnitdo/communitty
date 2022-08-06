79f83fba707ec3e06a0f1a1423d13f7530dcba4e
arnitdo <arnav.nitin.deo@gmail.com>
2022-08-06 14:42:49 +0530

    Cleanup url.ts file > common.ts

--- utils/url.ts
+++
@@ -1,7 +0,0 @@
-export function baseURL(): string {
-    if (process.env.NODE_ENV === "development"){
-        return "http://localhost:8800"
-    } else {
-        return "https://communitty.herokuapp.com"
-    }
-}
