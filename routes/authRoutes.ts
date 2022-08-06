ba2b2a2616dbd0a88f0cb9d854fc9a6c7622eac8
arnitdo <arnav.nitin.deo@gmail.com>
2022-08-06 14:45:20 +0530

    Change Auth validity 1d -> 1h | Refresh validity 7d -> 1d

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -42,7 +42,8 @@ function validateFullName(fullName: string): boolean {
     }
     return false
 }
-function generateAuthAndRefreshTokens(userName: string): [object, object]{
+
+function generateAuthAndRefreshTokens(userName: string): [object, object] {
     const signedAuthToken = sign(
         {
             userName: userName,
@@ -51,7 +52,7 @@ function generateAuthAndRefreshTokens(userName: string): [object, object]{
         // @ts-ignore
         process.env.JWT_SECRET,
         {
-            expiresIn: "1d"
+            expiresIn: "1h"
         }
     )
 
@@ -63,13 +64,12 @@ function generateAuthAndRefreshTokens(userName: string): [object, object]{
         // @ts-ignore
         process.env.JWT_SECRET,
         {
-            expiresIn: "7d"
+            expiresIn: "1d"
         }
     )
 
     return [signedAuthToken, signedRefreshToken]
 }
-
 
 
 async function isDuplicateUser(userName: string, userMail: string): Promise<boolean[]> {
@@ -91,7 +91,7 @@ async function isDuplicateUser(userName: string, userMail: string): Promise<bool
     return [isDuplicateUserName, isDuplicateUserMail]
 }
 
-function validateProperties(properties: object, propertyValidators: propertyValidatorType[]){
+function validateProperties(properties: object, propertyValidators: propertyValidatorType[]) {
     // A generic property validating function
     // Returns two arrays, valid and invalid properties
     let validProperties: string[] = [],     // List of valid properties
@@ -111,13 +111,13 @@ function validateProperties(properties: object, propertyValidators: propertyVali
     return [validProperties, invalidProperties]
 }
 
-function generateVerificationToken(){
+function generateVerificationToken() {
     // Length of token to generate
     const tokenLength = 64
     let verificationToken = ""
     // Set of characters to generate token from
     const charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    for (let idx = 0; idx < tokenLength; idx++){
+    for (let idx = 0; idx < tokenLength; idx++) {
         let randomIndex = Math.floor(
             Math.random() * (charSet.length - 1)
         )
@@ -128,276 +128,196 @@ function generateVerificationToken(){
 
 async function userSignup(req: Request, res: Response): Promise<void> {
     try {
-        if (req.body && req.body.userName && req.body.userMail && req.body.userPass && req.body.fullName) { // Check if all parameters are passed
-            const {userName, userMail, userPass, fullName} = req.body // Get required request parameters
-            const requestProperties: object = {     // Key-Value map of property name : property value
-                userName: userName,
-                userMail: userMail,
-                userPass: userPass,
-                fullName: fullName
-            }
-            const propertyValidators: propertyValidatorType[] = [   // List of property validator functions
-                validateUsername, validateEmail, validatePassword, validateFullName
-            ]
-            // Get valid and invalid properties
-            const [validProperties, invalidProperties] = validateProperties(requestProperties, propertyValidators)
-            if (invalidProperties.length > 0) {
-                // If there are some invalid properties, respond with 400
-                res.status(400).json(
-                    {
-                        "actionResult": "FAILURE",
-                        "validProperties": validProperties,      // Send valid and invalid properties
-                        "invalidProperties": invalidProperties,
-                        "duplicateProperties" : []
-                    }
-                )
-            } else {
-                await db.query("BEGIN;") // Begin DB Transaction
-                const [isDuplicateUserName, isDuplicateUserMail] = await isDuplicateUser(userName, userMail)
-                if (isDuplicateUserName || isDuplicateUserMail){
-                    let duplicateProperties: string[] = []
-                    if (isDuplicateUserName){
-                        duplicateProperties.push("userName")
-                    }
-                    if (isDuplicateUserMail){
-                        duplicateProperties.push("userMail")
-                    }
-                    res.status(400).json({
-                        "actionResult": "DUPLICATE",
-                        "validProperties" : validProperties,
-                        "invalidProperties": invalidProperties,
-                        "duplicateProperties": duplicateProperties
-                    })
-                } else {
-                    const passHash: string = await hash(userPass, 10)
-                    const verificationToken = generateVerificationToken()
-                    await db.query(
-                        "INSERT INTO users VALUES ($1, $2, $3, $4, NOW(), DEFAULT);",
-                        [userName, userMail, passHash, fullName]
-                    )
-                    await db.query(
-                        "INSERT INTO inactive_users VALUES ($1, $2);",
-                        [userName, verificationToken]
-                    )
-                    // Send verification mail
-                    await sendVerificationMail(userMail, userName, verificationToken)
-                    await db.query("COMMIT;")      // Commit transaction to db
-                    res.status(200).json({
-                        "actionResult": "SUCCESS",                // Notify of success
-                        "validProperties": validProperties,      // Send valid and invalid properties
-                        "invalidProperties": invalidProperties,
-                        "duplicateProperties": []
-                    })
-                }
-            }
-        } else {
+        const {userName, userMail, userPass, fullName} = req.body // Get required request parameters
+        const requestProperties: object = {     // Key-Value map of property name : property value
+            userName,
+            userMail,
+            userPass,
+            fullName
+        }
+        const propertyValidators: propertyValidatorType[] = [   // List of property validator functions
+            validateUsername, validateEmail, validatePassword, validateFullName
+        ]
+        // Get valid and invalid properties
+        const [validProperties, invalidProperties] = validateProperties(requestProperties, propertyValidators)
+        if (invalidProperties.length > 0) {
+            // If there are some invalid properties, respond with 400
             res.status(400).json(
                 {
-                    "actionResult": "FAILURE",
-                    "validProperties": [],
-                    "invalidProperties": ["userName", "userMail", "userPass", "fullName"],
-                    "duplicateProperties" : []
+                    "actionResult": "ERR_INVALID_PROPERTIES", // Send valid and invalid properties
+                    "invalidProperties": invalidProperties
                 }
             )
+        } else {
+            await db.query("BEGIN;") // Begin DB Transaction
+            const [isDuplicateUserName, isDuplicateUserMail] = await isDuplicateUser(userName, userMail)
+            if (isDuplicateUserName || isDuplicateUserMail) {
+                let duplicateProperties: string[] = []
+                if (isDuplicateUserName) {
+                    duplicateProperties.push("userName")
+                }
+                if (isDuplicateUserMail) {
+                    duplicateProperties.push("userMail")
+                }
+                res.status(400).json({
+                    "actionResult": "ERR_DUPLICATE_PROPERTIES",
+                    "duplicateProperties": duplicateProperties
+                })
+            } else {
+                const passHash: string = await hash(userPass, 10)
+                const verificationToken = generateVerificationToken()
+                await db.query(
+                    "INSERT INTO users VALUES ($1, $2, $3, $4, NOW(), DEFAULT);",
+                    [userName, userMail, passHash, fullName]
+                )
+                await db.query(
+                    "INSERT INTO inactive_users VALUES ($1, $2);",
+                    [userName, verificationToken]
+                )
+                // Send verification mail
+                await sendVerificationMail(userMail, userName, verificationToken)
+                await db.query("COMMIT;")      // Commit transaction to db
+                res.status(200).json({
+                    "actionResult": "SUCCESS"                 // Notify of success
+                })
+            }
         }
     } catch (err) {
         console.error(err)
         await db.query("ROLLBACK;")
         res.status(500).json({
-            "actionResult": "ERROR",
-            "validProperties": [],
-            "invalidProperties": [],
-            "duplicateProperties": []
+            "actionResult": "ERR_INTERNAL_ERROR"
         })
     }
 }
 
-async function userVerify(req: Request, res: Response): Promise<void>{
+async function userActivate(req: Request, res: Response): Promise<void> {
     try {
-        if (req.body && req.body.userName && req.body.verificationToken) {
-            const {userName, verificationToken} = req.body
-            await db.query("BEGIN;")
-            const {rows} = await db.query(
-                "DELETE FROM inactive_users WHERE username = $1 AND verificationtoken = $2 RETURNING *",
-                [userName, verificationToken]
+        const {userName, activationToken} = req.body
+        await db.query("BEGIN;")
+        const {rows} = await db.query(
+            "DELETE FROM inactive_users WHERE username = $1 AND activationToken = $2 RETURNING *",
+            [userName, activationToken]
+        )
+        if (rows.length > 0) {
+            // Activate our user
+            await db.query(
+                "UPDATE users SET activated = True WHERE username = $1",
+                [userName]
             )
-            if (rows.length > 0) {
-                // Activate our user
-                await db.query(
-                    "UPDATE users SET activated = True WHERE username = $1",
-                    [userName]
-                )
-                res.status(200).json({
-                    "actionResult": "SUCCESS",
-                    "validProperties": ["userName", "verificationToken"],
-                    "invalidProperties": []
-                })
-            } else {
-                res.status(400).json({
-                    "actionResult": "FAILURE",
-                    "validProperties": [],
-                    "invalidProperties": ["userName", "verificationToken"]
-                })
-            }
-            await db.query("COMMIT;")
+            res.status(200).json({
+                "actionResult": "SUCCESS"
+            })
         } else {
             res.status(400).json({
-                "actionResult": "FAILURE",
-                "validProperties": [],
-                "invalidProperties": ["userName", "verificationToken"]
+                "actionResult": "ERR_INVALID_PROPERTIES",
+                "invalidProperties": ["userName", "activationToken"]
             })
         }
-    } catch (err){
+        await db.query("COMMIT;")
+    } catch (err) {
         console.error(err)
         res.status(500).json({
-            "actionResult": "ERROR",
-            "validProperties": [],
-            "invalidProperties": []
+            "actionResult": "ERR_INTERNAL_ERROR",
         })
         await db.query("ROLLBACK;")
     }
 }
 
-async function userLogin(req: Request, res: Response): Promise<void>{
+async function userLogin(req: Request, res: Response): Promise<void> {
     try {
-        if (req.body && req.body.userName && req.body.userPass){
-            const {userName, userPass} = req.body
-            const {rows} = await db.query(
-                "SELECT username, password FROM users WHERE username = $1",
-                [userName]
-            )
+        const {userName, userPass} = req.body
+        const {rows} = await db.query(
+            "SELECT username, password FROM users WHERE username = $1 LIMIT 1",
+            [userName]
+        )
 
-            if (rows.length == 0){
-                res.status(400).json({
-                    "actionResult": "FAILURE",
-                    "validProperties": [],
-                    "invalidProperties": ["userName", "userPass"],
-                    "authToken": null
-                })
-            }
-
-            const {username, password} = rows[0]
-
-            const passwordMatch = await compare(userPass, password)
-
-            if (passwordMatch == true){
-
-                const [signedAuthToken, signedRefreshToken] = generateAuthAndRefreshTokens(username)
-
-                res.status(200).json({
-                    "actionResult": "SUCCESS",
-                    "validProperties": ["userName", "userPass"],
-                    "invalidProperties": [],
-                    "authToken": signedAuthToken,
-                    "refreshToken": signedRefreshToken
-                })
-            } else {
-                res.status(200).json({
-                    "actionResult": "FAILURE",
-                    "validProperties": ["userName"],
-                    "invalidProperties": ["userPass"],
-                    "authToken": null,
-                    "refreshToken": null
-                })
-            }
-        } else {
+        if (rows.length == 0) {
             res.status(400).json({
-                "actionResult": "FAILURE",
-                "validProperties": [],
-                "invalidProperties": ["userName", "userPass"],
-                "authToken": null,
-                "refreshToken": null
+                "actionResult": "ERR_INVALID_PROPERTIES",
+                "invalidProperties": ["userName", "userPass"]
+            })
+            return
+        }
+
+        const {username, password} = rows[0]
+
+        const passwordMatch = await compare(userPass, password)
+
+        if (passwordMatch == true) {
+
+            const [signedAuthToken, signedRefreshToken] = generateAuthAndRefreshTokens(username)
+
+            res.status(200).json({
+                "actionResult": "SUCCESS",
+                "authToken": signedAuthToken,
+                "refreshToken": signedRefreshToken
+            })
+        } else {
+            res.status(200).json({
+                "actionResult": "ERR_INVALID_PROPERTIES",
+                "invalidProperties": ["userPass"],
             })
         }
-    } catch (err){
+    } catch (err) {
         console.error(err)
         res.status(500).json({
-            "actionResult": "ERROR",
-            "validProperties": [],
-            "invalidProperties": [],
-            "authToken" : null,
-            "refreshToken": null
+            "actionResult": "ERR_INTERNAL_ERROR"
         })
     }
 }
 
-async function userTokenRefresh(req: Request, res: Response): Promise<void>{
+async function userTokenRefresh(req: Request, res: Response): Promise<void> {
     try {
-        if (req.body && req.body.authToken && req.body.refreshToken && req.body.authToken != req.body.refreshToken){
-            const {authToken, refreshToken} = req.body
-            const [authTokenValidity, authTokenData] = validateAuthToken(authToken)
+        const {authToken, refreshToken} = req.body
+        const [authTokenValidity, authTokenData] = validateAuthToken(authToken)
 
-            if (authTokenData == null){
-                res.status(400).json({
-                    "actionResult": "FAILURE",
-                    "validProperties": [],
-                    "invalidProperties": ["authToken"],
-                    "authToken": null,
-                    "refreshToken": null
-                })
-
-                return
-            }
-
-            // @ts-ignore
-            const tokenUserName = authTokenData.userName
-
-            const [refreshTokenValidity, refreshTokenData] = validateRefreshToken(refreshToken, tokenUserName)
-
-            if (refreshTokenData == null){
-                res.status(400).json({
-                    "actionResult": "FAILURE",
-                    "validProperties": [],
-                    "invalidProperties": ["refreshToken"],
-                    "authToken": null,
-                    "refreshToken": null
-                })
-            }
-
-            if  (refreshTokenValidity == true){
-                const [newAuthToken, newRefreshToken] = generateAuthAndRefreshTokens(tokenUserName)
-
-                res.status(200).json({
-                    "actionResult": "SUCCESS",
-                    "validProperties": ["authToken", "refreshToken"],
-                    "invalidProperties": [],
-                    "authToken": newAuthToken,
-                    "refreshToken": newRefreshToken
-                })
-            } else {
-                res.status(200).json({
-                    "actionResult": "REFRESH_EXPIRED",
-                    "validProperties": ["authToken", "refreshToken"],
-                    "invalidProperties": [],
-                    "authToken": null,
-                    "refreshToken": null
-                })
-            }
-
-        } else {
+        if (authTokenData == null) {
             res.status(400).json({
-                "actionResult": "FAILURE",
-                "validProperties": [],
-                "invalidProperties": ["authToken", "refreshToken"],
-                "authToken": null,
-                "refreshToken": null
+                "actionResult": "ERR_INVALID_PROPERTIES",
+                "invalidProperties": ["authToken"],
+            })
+            return
+        }
+
+        // @ts-ignore
+        const tokenUserName = authTokenData.userName
+
+        const [refreshTokenValidity, refreshTokenData] = validateRefreshToken(refreshToken, tokenUserName)
+
+        if (refreshTokenData == null) {
+            res.status(400).json({
+                "actionResult": "ERR_INVALID_PROPERTIES",
+                "invalidProperties": ["refreshToken"]
             })
         }
-    } catch (err){
+
+        if (refreshTokenValidity == true) {
+            const [newAuthToken, newRefreshToken] = generateAuthAndRefreshTokens(tokenUserName)
+
+            res.status(200).json({
+                "actionResult": "SUCCESS",
+                "authToken": newAuthToken,
+                "refreshToken": newRefreshToken
+            })
+        } else {
+            // Isn't exactly an error, the client will simply have to relogin
+            // Hence status 200. A completely invalid refresh token will still return 400
+            res.status(200).json({
+                "actionResult": "REFRESH_EXPIRED",
+            })
+        }
+    } catch (err) {
         console.error(err);
         res.status(500).json({
-            "actionResult": "ERROR",
-            "validProperties": [],
-            "invalidProperties": [],
-            "authToken" : null,
-            "refreshToken": null
+            "actionResult": "ERR_INTERNAL_ERROR"
         })
     }
 }
 
 export {
     userSignup,
-    userVerify,
+    userActivate,
     userLogin,
     userTokenRefresh
 }
