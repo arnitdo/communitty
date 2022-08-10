3c0c91faa33dacb7f118bbdecd4934fb0d24a3db
arnitdo <arnav.nitin.deo@gmail.com>
2022-08-10 21:08:15 +0530

    Refactor verified -> activated

--- utils/emailService.ts
+++ utils/emailService.ts
@@ -5,20 +5,20 @@ const emailService = sendGrid
 // @ts-ignore
 emailService.setApiKey(process.env.SENDGRID_API_KEY)
 
-async function sendVerificationMail(emailRecipient: string, userName: string, verificationToken: string): Promise<void> {
-    // Creates and sends a verification email from the passed parameters
+async function sendActivationMail(emailRecipient: string, userName: string, activationToken: string): Promise<void> {
+    // Creates and sends an activation email from the passed parameters
     // Uses Twilio's SendGrid mail API
 
     // Encode username
     const urlEncodedUsername: string = encodeURIComponent(userName)
-    // encode the verification token, as it contains special chars
-    const urlEncodedToken: string = encodeURIComponent(verificationToken)
+    // encode the activation token, as it contains special chars
+    const urlEncodedToken: string = encodeURIComponent(activationToken)
     // Get base URL (In case of dev / prod environments)
     const baseURLString: string = baseURL()
-    // Concatenate to form verification URL
-    const verificationURL: string = baseURLString + "/activate?userName=" + urlEncodedUsername + "&verificationToken=" + urlEncodedToken
-    // Craft verification email
-    const verificationMessage: object = {
+    // Concatenate to form activation URL
+    const activationURL: string = baseURLString + "/activate?userName=" + urlEncodedUsername + "&activationToken=" + urlEncodedToken
+    // Craft activation email
+    const activationMessage: object = {
         to: emailRecipient,
         from: "join.communitty@gmail.com",
         subject: "Activate your Communitty account now!",
@@ -26,16 +26,16 @@ async function sendVerificationMail(emailRecipient: string, userName: string, ve
             `Thank you for joining Communitty! There's still one small step left.`  +
             `\n\nClick the link below to activate your account on Communitty.`      +
             `\nAn activated account permits you to post, comment and more!`         +
-            `\n\n${verificationURL}`
+            `\n\n${activationURL}`
     }
     try {
         // @ts-ignore
         // And sendoff!
-        const emailResponse = await emailService.send(verificationMessage)
+        const emailResponse = await emailService.send(activationMessage)
     } catch (err) {
         // Log the error, if any
         console.error(err)
     }
 }
 
-export {sendVerificationMail}
+export {sendActivationMail}
