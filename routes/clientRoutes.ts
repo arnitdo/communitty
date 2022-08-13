63ae791fe8b5a13c51cdcbcf3da721d42947918f
arnitdo <arnav.nitin.deo@gmail.com>
2022-08-13 21:02:05 +0530

    Add API 404 route

--- routes/clientRoutes.ts
+++ routes/clientRoutes.ts
@@ -1,10 +1,17 @@
 import * as path from "path"
 import {Request, Response} from "express";
 
+function apiNotFound(req: Request, res: Response){
+	res.sendStatus(404)
+}
+
 function serveClient(req: Request, res: Response){
 	res.status(200).sendFile(
 		path.join(__dirname, '../client/build/index.html')
 	)
 }
 
-export {serveClient}
+export {
+	apiNotFound,
+	serveClient
+}
