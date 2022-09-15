9e75a0a7a590d4a6a335862141850a29347ee6a5
arnitdo <arnav.nitin.deo@gmail.com>
2022-09-15 19:08:37 +0530

    Add user profile, posts and comments routes

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -18,6 +18,45 @@ async function redirectToUserProfile(req: Request, res: Response): Promise<void>
 	}
 }
 
+async function getUserProfile(req: Request, res: Response): Promise<void> {
+	try {
+		// userName is guaranteed with needsURLParams middleware
+		const {userName} = req.params
+
+		const {rows} = await db.query(
+			"SELECT * FROM profiles WHERE username = $1;",
+			[userName]
+		)
+
+		if (rows.length == 0){
+			// No user exists with that username
+			res.sendStatus(404)
+			return
+		}
+
+		const userProfileData = rows[0]
+
+		const normalizedUserProfileData = normalizeObjectKeys(userProfileData)
+
+		const finalUserData = {
+			// We'll be adding a few helpful hints to other profile data
+			...normalizedUserProfileData,
+			"userPosts": `/api/users/${userName}/posts`,
+			"userComments": `/api/users/${userName}/comments`
+		}
+
+		res.status(200).json({
+			"actionResult": "SUCCESS",
+			"profileData": finalUserData
+		})
+	} catch (err){
+		console.error(err)
+		res.status(500).json({
+			"actionResult": "ERR_INTERNAL_ERROR"
+		})
+	}
+}
+
 async function getUserPosts(req: Request, res: Response): Promise<void> {
 	try {
 		const userName = req.params.userName
@@ -57,6 +96,47 @@ async function getUserPosts(req: Request, res: Response): Promise<void> {
 	}
 }
 
+async function getUserComments(req: Request, res: Response): Promise<void> {
+	try {
+		// req.params.userName is guaranteed with needsURLParams middleware
+		const {userName} = req.params
+		const commentPage = req.query.commentPage as string || "1"
+		const parsedCommentPage = Number.parseInt(commentPage)
+		if (Number.isNaN(parsedCommentPage) || parsedCommentPage < 0){
+			res.status(400).json({
+				"actionResult": "ERR_INVALID_PROPERTIES",
+				"invalidProperties": ["commentPage"]
+			})
+			return
+		}
+
+		const pageOffset = ((parsedCommentPage - 1) * 10)
+
+		const {rows} = await db.query(
+			"SELECT * FROM comments WHERE comment_author = $1 ORDER BY comment_modified_time DESC LIMIT 10 OFFSET $2;",
+			[userName, pageOffset]
+		)
+
+		const normalizedRows = rows.map((row) => {
+			return normalizeObjectKeys(
+				row,
+				['comment_modified_time']
+			)
+		})
+
+		res.status(200).json({
+			"actionResult": "SUCCESS",
+			"userComments": normalizedRows
+		})
+	} catch (err){
+		console.error(err)
+		res.status(500).json({
+			"actionResult": "ERR_INTERNAL_ERROR"
+		})
+	}
+}
+
+
 const userRouter = Router()
 
 // Redirects to the profile of the currently authenticated user
@@ -71,11 +151,27 @@ userRouter.get(
 )
 
 userRouter.get(
+	"/:userName/profile",
+	[
+		middleware.needsURLParams("userName")
+	],
+	getUserProfile
+)
+
+userRouter.get(
 	"/:userName/posts",
 	[
 		middleware.needsURLParams("userName")
 	],
 	getUserPosts
+)
+
+userRouter.get(
+	"/:userName/comments",
+	[
+		middleware.needsURLParams("userName")
+	],
+	getUserComments
 )
 
 export {
