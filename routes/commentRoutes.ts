09ffcb6da87c5471695cc4ff060f37ea0708ec39
arnitdo <arnav.nitin.deo@gmail.com>
2022-09-18 15:39:18 +0530

    Implement comment likes/dislikes

--- routes/commentRoutes.ts
+++ routes/commentRoutes.ts
@@ -8,7 +8,7 @@ const validCommentTypes = ["ROOT", "REPLY"]
 async function getDirectChildComments(commentId: string): Promise<string[]> {
 	// returns comment IDs of all direct children of a specific comment
 	const {rows} = await db.query(
-		"SELECT * FROM comments WHERE comment_reply_parent = $1",
+		"SELECT comment_id FROM comments WHERE comment_reply_parent = $1",
 		[commentId]
 	)
 	const childComments = rows.map((row) => {
@@ -17,7 +17,7 @@ async function getDirectChildComments(commentId: string): Promise<string[]> {
 	return childComments
 }
 
-async function getRecursiveChildComments(commentId: string): Promise<Object> {
+async function getRecursiveChildComments(commentId: string, currentUser: string | null): Promise<Object> {
 	// Returns comment IDs of all children / grand-children / grand-grand.... you get the point
 	const directChildren = await getDirectChildComments(commentId)
 	const commentTree = await Promise.all(
@@ -35,9 +35,23 @@ async function getRecursiveChildComments(commentId: string): Promise<Object> {
 				["comment_modified_time"]
 			)
 
+			let commentLikeStatus: boolean = false
+
+			if (currentUser != null){
+				const {rows} = await db.query(
+					"SELECT 1 FROM comment_likes WHERE comment_id = $1 AND username = $2;",
+					[childComment, currentUser]
+				)
+
+				if (rows.length == 1){
+					commentLikeStatus = true
+				}
+			}
+
 			const packedTreeData = {
 				...normalizedCommentData,
-				"childComments": await getRecursiveChildComments(childComment)
+				"userLikeStatus": commentLikeStatus,
+				"childComments": await getRecursiveChildComments(childComment, currentUser)
 			}
 
 			return packedTreeData
@@ -164,18 +178,39 @@ async function getPostComments(req: Request, res: Response): Promise<void> {
 			[postId, commentPageOffset]
 		)
 
+		let currentUser: string | null = null
+
+		if (req.header("Authorization")){
+			currentUser = getAuthenticatedUser(req)
+		}
+
+
 		const childCommentTree = await Promise.all(
 			rows.map(async (commentData) => {
 				const commentId = commentData.comment_id
-				const commentTree = await getRecursiveChildComments(commentId)
+				const commentTree = await getRecursiveChildComments(commentId, currentUser)
 
 				const normalizedCommentData = normalizeObjectKeys(
 					commentData,
 					["comment_modified_time"]
 				)
 
+				let commentLikeStatus: boolean = false
+
+				if (currentUser != null){
+					const {rows} = await db.query(
+						"SELECT 1 FROM comment_likes WHERE comment_id = $1 AND username = $2;",
+						[commentId, currentUser]
+					)
+
+					if (rows.length == 1){
+						commentLikeStatus = true
+					}
+				}
+
 				return {
 					...normalizedCommentData,
+					"userLikeStatus": commentLikeStatus,
 					"childComments": commentTree
 				}
 			})
@@ -219,17 +254,108 @@ async function getComment(req: Request, res: Response): Promise<void> {
 			[commentId]
 		)
 
+		let currentUser: string | null = null
+		let commentLikeStatus: boolean = false
+
+		if (req.header("Authorization")){
+			currentUser = getAuthenticatedUser(req)
+
+			const {rows} = await db.query(
+				"SELECT 1 FROM comment_likes WHERE comment_id = $1 AND username = $2;",
+				[commentId, currentUser]
+			)
+
+			if (rows.length == 1){
+				commentLikeStatus = true
+			}
+		}
+
 		const childComments = await getDirectChildComments(commentId)
 		const commentData = rows[0]
-		let normalizedCommentData = normalizeObjectKeys(
+		const normalizedCommentData = normalizeObjectKeys(
 			commentData,
 			["comment_modified_time"]
 		)
-		normalizedCommentData["childComments"] = childComments
+
+		const finalCommentData = {
+			...normalizedCommentData,
+			"userLikeStatus": commentLikeStatus,
+			"childComments": childComments
+		}
+
 		res.status(200).json({
 			"actionResult": "SUCCESS",
-			"commentData": normalizedCommentData
+			"commentData": finalCommentData
 		})
+	} catch (err){
+		console.error(err)
+		res.status(500).json({
+			"actionResult": "ERR_INTERNAL_ERROR"
+		})
+	}
+}
+
+async function getCommentTree(req: Request, res: Response): Promise<void> {
+	// GET /api/comments/:commentId/tree
+	// Returns a tree like structure of the given commentId
+	try {
+		const {commentId} = req.params
+
+		const postId = req.params.postId
+		if (postId){
+			// If a post id is passed as URL param, ensure that the comment is in the post tree
+			const {rows} = await db.query(
+				"SELECT 1 FROM comments WHERE comment_id = $1 AND comment_parent_post = $2",
+				[commentId, postId]
+			)
+			if (rows.length == 0){
+				res.status(400).json({
+					"actionResult": "ERR_INVALID_PROPERTIES",
+					"invalidProperties": ["postId", "commentId"]
+				})
+				return
+			}
+			// Comment is in post tree, continue fetching
+		}
+
+		const {rows} = await db.query(
+			"SELECT * FROM comments WHERE comment_id = $1;",
+			[commentId]
+		)
+		const commentData = rows[0]
+
+		let currentUser: string | null = null
+		let commentLikeStatus: boolean = false
+
+		if (req.header("Authorization")) {
+			currentUser = getAuthenticatedUser(req)
+
+			const {rows} = await db.query(
+				"SELECT 1 FROM comment_likes WHERE comment_id = $1 AND username = $2;",
+				[commentId, currentUser]
+			)
+
+			if (rows.length == 1){
+				commentLikeStatus = true
+			}
+		}
+
+		const normalizedCommentData = normalizeObjectKeys(
+			commentData,
+			['comment_modified_time']
+		)
+
+		const finalCommentData = {
+			...normalizedCommentData,
+			"userLikeStatus": commentLikeStatus,
+			"childComments": await getRecursiveChildComments(commentId, currentUser)
+		}
+
+		res.status(200).json({
+			"actionResult": "SUCCESS",
+			"commentData": finalCommentData
+		})
+
 	} catch (err){
 		console.error(err)
 		res.status(500).json({
@@ -279,6 +405,63 @@ async function updateComment(req: Request, res: Response): Promise<void> {
 	} catch (err){
 		console.error(err)
 		await db.query("ROLLBACK;")
+		res.status(500).json({
+			"actionResult": "ERR_INTERNAL_ERROR"
+		})
+	}
+}
+
+async function getCommentLikes(req: Request, res: Response): Promise<void> {
+	try {
+		const {commentId} = req.params
+		const postId = req.params.postId
+
+		if (postId){
+			// Validate the comment & comment parent match if postId is parent
+			const {rows} = await db.query(
+				"SELECT 1 FROM comments WHERE comment_id = $1 AND comment_parent_post = $2",
+				[commentId, postId]
+			)
+
+			if (rows.length == 0){
+				res.status(400).json({
+					"actionResult": "ERR_INVALID_PROPERTIES",
+					"invalidProperties": ["commentId", "postId"]
+				})
+
+				return
+			}
+		}
+
+		const likePage = req.query.likePage as string || "1"
+
+		const parsedLikePage = Number.parseInt(likePage)
+		if (Number.isNaN(parsedLikePage) || parsedLikePage < 1){
+			res.status(400).json({
+				"actionResult": "ERR_INVALID_PROPERTIES",
+				"invalidProperties": ["likePage"]
+			})
+			return
+		}
+
+		const pageOffset = ((parsedLikePage - 1) * 10)
+
+		const {rows} = await db.query(
+			"SELECT username FROM comment_likes WHERE comment_id = $1 OFFSET $2 LIMIT 10;",
+			[commentId, pageOffset]
+		)
+
+		const likedUsers = rows.map((row) => {
+			return row.username
+		})
+
+		res.status(200).json({
+			"actionResult": "SUCCESS",
+			"likedUsers": likedUsers
+		})
+
+	} catch (err) {
+		console.error(err)
 		res.status(500).json({
 			"actionResult": "ERR_INTERNAL_ERROR"
 		})
@@ -387,6 +570,10 @@ const commentRouter = Router({
  * 			- Creates a comment on specific post + verifies parent post validity
  * 		- PUT /:commentId/:
  * 			- Updates the specific comment
+ * 		- POST /:commentId/likes:
+ * 			- Likes the specific comment
+ * 		- DELETE /:commentId/likes:
+ * 			- Dislikes the specific comment
  *	- /api/comments/
  * 		- GET /:
  * 			- Errors due to incorrect route (see above)
@@ -396,6 +583,10 @@ const commentRouter = Router({
  * 			- Creates a comment with the specified postId (body param) as the parent post
  * 		- PUT /:commentId/:
  * 			- Updates the specific comment
+ * 		- POST /:commentId/likes:
+ * 			- Likes the specific comment
+ * 		- DELETE /:commentId/likes:
+ * 			- Dislikes the specific comment
  */
 
 commentRouter.post(
@@ -417,9 +608,20 @@ commentRouter.get(
 	"/:commentId/",
 	[
 		middleware.needsURLParams("commentId"),
-		middleware.needsValidComment
+		middleware.needsValidComment,
+		middleware.optionalToken
 	],
 	getComment
+)
+
+commentRouter.get(
+	"/:commentId/tree",
+	[
+		middleware.needsURLParams("commentId"),
+		middleware.needsValidComment,
+		middleware.optionalToken
+	],
+	getCommentTree
 )
 
 commentRouter.put(
@@ -432,6 +634,35 @@ commentRouter.put(
 		middleware.needsBodyParams("commentBody")
 	],
 	updateComment
+)
+
+commentRouter.get(
+	"/:commentId/likes",
+	[
+		middleware.needsURLParams("commentId"),
+		middleware.needsValidComment
+	],
+	getCommentLikes
+)
+
+commentRouter.post(
+	"/:commentId/likes",
+	[
+		middleware.needsToken,
+		middleware.needsURLParams("commentId"),
+		middleware.needsValidComment
+	],
+	likeComment
+)
+
+commentRouter.delete(
+	"/:commentId/likes",
+	[
+		middleware.needsToken,
+		middleware.needsURLParams("commentId"),
+		middleware.needsValidComment
+	],
+	dislikeComment
 )
 
 export {
