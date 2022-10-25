cfb22d2db1cc5b8b8606263e4351d92b912d935c
arnitdo <arnav.nitin.deo@gmail.com>
2022-10-25 21:16:52 +0530

    Add auth state refresh trigger

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,21 @@ import * as React from 'react'
 import {Helmet} from 'react-helmet-async'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import {TopBar} from "./components/topbar";
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import {HomeFeed} from "./pages/homeFeed";
 import {Spacer} from "@chakra-ui/react";
+import {LoginPage} from "./pages/loginPage";
 
 function App(): JSX.Element {
 	const [showTopBar, setShowTopBar] = useState<boolean>(true)
+
+	const [authState, setAuthState] = useState<number>(0)
+
+	// Basic function to trigger a re-render of auth state based components.
+	// Useful when forcing a re-render of
+	const refreshAuth = useCallback(() => {
+		setAuthState(authState + 1)
+	}, [authState])
 
 	return (
 		<>
@@ -27,11 +36,23 @@ function App(): JSX.Element {
 			{/*Routing*/}
 			<BrowserRouter>
 				{showTopBar ? (
-					<TopBar/>
+					<TopBar
+						authState={authState}
+						refreshAuth={() => refreshAuth()}
+					/>
 				) : null}
 				<Spacer height={"2vh"} />
 				<Routes>
-					<Route path={"/"} element={<HomeFeed />}></Route>
+					<Route
+						path={"/"}
+						element={<HomeFeed />}
+					/>
+					<Route
+						path={"login"}
+						element={<LoginPage
+							refreshAuth={() => refreshAuth()}
+						/>}
+					/>
 				</Routes>
 			</BrowserRouter>
 		</>
