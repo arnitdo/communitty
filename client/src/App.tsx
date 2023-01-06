8fb2dbeac4bd274b8d5b3f6b0ac3eee151a64ba1
arnitdo <arnav.nitin.deo@gmail.com>
2023-01-06 19:12:08 +0530

    Refactor account activation toast & Signup Routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,12 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import {TopBar} from "./components/topbar";
 import {useCallback, useEffect, useState} from "react";
 import {HomeFeed} from "./pages/homeFeed";
-import {Spacer, useToast} from "@chakra-ui/react";
+import {Spacer, useColorMode, useToast} from "@chakra-ui/react";
 import {LoginPage} from "./pages/loginPage";
 import {ProfileContext} from "./utils/profileContext";
 import {ProfileContextType} from "./utils/typeDefs";
 import {useAPIRequest} from "./utils/apiHandler";
+import {SignupPage} from "./pages/signupPage";
 
 function App(): JSX.Element {
 	const [showTopBar, setShowTopBar] = useState<boolean>(true)
@@ -31,24 +32,30 @@ function App(): JSX.Element {
 				if (actionResult === "SUCCESS") {
 					setProfileValue(profileData)
 				}
-				if (!profileData.accountActivated){
-					showToast({
-						status: "warning",
-						isClosable: true,
-						title: "Account activation is incomplete",
-						description: "Follow the steps in the activation mail sent to your account"
-					})
-				}
 			} else {
 				setProfileValue(null)
 			}
 		}
 	}, [authState, isLoading])
+
+	useEffect(() => {
+		if (profileValue && profileValue.accountActivated === false){
+			showToast({
+				status: "warning",
+				isClosable: true,
+				title: "Account activation is incomplete",
+				description: "Follow the steps in the activation mail sent to your account"
+			})
+		}
+	}, [profileValue])
+
 	// Basic function to trigger a re-render of auth state based components.
 	// Useful when forcing a re-render of auth based components
 	const refreshAuth = useCallback(() => {
 		setAuthState(authState + 1)
 	}, [authState])
+
+	const {colorMode} = useColorMode()
 
 	return (
 		<ProfileContext.Provider value={profileValue}>
@@ -63,17 +70,19 @@ function App(): JSX.Element {
 					name={"keywords"}
 					content={"communitty,open source,community,social media"}
 				></meta>
+				<link rel={"icon"} href={`favicon-${colorMode}.ico`} />
 			</Helmet>
 
 			{/*Routing*/}
 			<BrowserRouter>
 				{showTopBar ? (
-					<TopBar
-						authState={authState}
-						refreshAuth={() => refreshAuth()}
-					/>
+					<>
+						<TopBar
+							authState={authState}
+							refreshAuth={() => refreshAuth()}
+						/>
+					</>
 				) : null}
-				<Spacer height={"2vh"} />
 				<Routes>
 					<Route
 						path={"/"}
@@ -88,6 +97,12 @@ function App(): JSX.Element {
 						path={"login"}
 						element={<LoginPage
 							refreshAuth={() => refreshAuth()}
+							setShowTopBar={setShowTopBar}
+						/>}
+					/>
+					<Route
+						path={"signup"}
+						element={<SignupPage
 							setShowTopBar={setShowTopBar}
 						/>}
 					/>
